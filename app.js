8583b4ce877c00c2b6b05c7062f2a93f61ed29b7
pekkenpah <akkapehonen@gmail.com>
2023-03-01 13:27:07 +0200

    4.1 versio

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+
+// yhdistetään kantaan
+logger.info('connecting to db...')
+mongoose.connect(config.MONGODB_URI).then(result => {
+    logger.info('connected to MongoDB.')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
+
+  app.use(cors())
+  app.use(express.static('build'))
+  app.use(express.json())
+  app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
